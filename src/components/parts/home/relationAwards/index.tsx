24efe3a8a2da8acd8df068effe98247d76054687
dennishaugen <dennishaugen77@gmail.com>
2025-08-26 14:27:28 +0200

    refactor: update relationAwards component for mobile responsive

--- src/components/parts/home/relationAwards/index.tsx
+++ src/components/parts/home/relationAwards/index.tsx
@@ -2,41 +2,43 @@ import { cn } from "@/utils/cn"
 
 export const RelationAwards = () => {
   return (
-    <div className="mx-auto flex w-full max-w-[1200px] flex-col px-10 py-24">
-      <p className="text-primary font-primary font-semibold uppercase">
-        Relation & Awards
-      </p>
+    <div className="flex w-full px-10">
+      <div className="mx-auto flex w-full max-w-[1200px] flex-col py-24">
+        <p className="text-primary font-primary font-semibold uppercase">
+          Relation & Awards
+        </p>
 
-      <div className="mt-11 flex flex-col gap-8">
-        <div className="grid grid-cols-1 items-center justify-center md:flex md:gap-8">
-          {logoListTop.map((el, index) => {
-            return (
-              <div
-                key={index}
-                className={cn(
-                  index === 0 && "shadow-[0px_0px_10px_0px_rgb(0,0,0,0.5)]",
-                  "mx-auto flex h-35 w-80 cursor-pointer items-center justify-center rounded-md transition-all duration-300 ease-in-out hover:shadow-[0px_24px_80px_-16px_rgba(0,0,0,0.15)]",
-                )}
-              >
-                <img src={el} width={120} height={80} />
-              </div>
-            )
-          })}
-        </div>
+        <div className="mt-11 flex flex-col gap-8">
+          <div className="grid grid-cols-1 items-center justify-center md:flex md:gap-8">
+            {logoListTop.map((el, index) => {
+              return (
+                <div
+                  key={index}
+                  className={cn(
+                    index === 0 && "shadow-[0px_0px_10px_0px_rgb(0,0,0,0.5)]",
+                    "mx-auto flex h-35 w-50 cursor-pointer items-center justify-center rounded-md transition-all duration-300 ease-in-out hover:shadow-[0px_24px_80px_-16px_rgba(0,0,0,0.15)] sm:w-80",
+                  )}
+                >
+                  <img src={el} width={120} height={80} />
+                </div>
+              )
+            })}
+          </div>
 
-        <div className="grid grid-cols-1 items-center justify-center md:flex md:gap-8">
-          {logoListBottom.map((el, index) => {
-            return (
-              <div
-                key={index}
-                className={cn(
-                  "mx-auto flex h-35 w-80 cursor-pointer items-center justify-center rounded-md transition-all duration-300 ease-in-out hover:shadow-[0px_24px_80px_-16px_rgba(0,0,0,0.15)]",
-                )}
-              >
-                <img src={el} width={120} height={80} />
-              </div>
-            )
-          })}
+          <div className="grid grid-cols-1 items-center justify-center md:flex md:gap-8">
+            {logoListBottom.map((el, index) => {
+              return (
+                <div
+                  key={index}
+                  className={cn(
+                    "mx-auto flex h-35 w-50 cursor-pointer items-center justify-center rounded-md transition-all duration-300 ease-in-out hover:shadow-[0px_24px_80px_-16px_rgba(0,0,0,0.15)] md:w-80",
+                  )}
+                >
+                  <img src={el} width={120} height={80} />
+                </div>
+              )
+            })}
+          </div>
         </div>
       </div>
     </div>
