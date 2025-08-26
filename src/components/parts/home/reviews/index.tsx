6b64755d025c6ef690810247b55af158cd1e71d0
dennishaugen <dennishaugen77@gmail.com>
2025-08-26 14:31:16 +0200

    refactor: update reviews component for mobile responsive

--- src/components/parts/home/reviews/index.tsx
+++ src/components/parts/home/reviews/index.tsx
@@ -25,43 +25,45 @@ export const Reviews = () => {
           )
         })}
       </div>
-      <div className="border-primary absolute top-0 left-1/2 grid w-full max-w-[800px] -translate-x-1/2 -translate-y-1/2 grid-cols-2 justify-between gap-5 rounded-lg border-2 bg-white p-16 md:flex md:max-w-[1200px]">
-        <div className="flex flex-col items-center md:flex-row">
-          <div className="bg-primary flex h-11 w-12 items-start justify-center rounded-[300%] md:h-22">
-            <img src="/svg/google.svg" width={40}></img>
+      <div className="absolute top-0 left-1/2 flex w-full -translate-x-1/2 -translate-y-1/2 px-10">
+        <div className="border-primary mx-auto grid w-full max-w-[800px] grid-cols-2 justify-between gap-5 rounded-lg border-2 bg-white p-6 md:flex md:max-w-[1200px] md:p-16">
+          <div className="flex flex-col items-center md:flex-row">
+            <div className="bg-primary flex h-11 w-12 items-start justify-center rounded-[300%] md:h-22">
+              <img src="/svg/google.svg" width={40}></img>
+            </div>
+            <div className="flex flex-col items-center md:ml-3">
+              <p className="font-secondary text-start text-3xl font-bold md:text-6xl">
+                4.8
+              </p>
+              <p className="font-secondary font-semibold">200+ Reviews</p>
+            </div>
           </div>
-          <div className="flex flex-col items-center md:ml-3">
-            <p className="font-secondary text-start text-3xl font-bold md:text-6xl">
-              4.8
-            </p>
-            <p className="font-secondary font-semibold">200+ Reviews</p>
+          <div className="flex flex-col items-center md:flex-row">
+            <div className="bg-primary font-secondary flex h-11 w-12 items-start justify-center rounded-[300%] md:h-22">
+              <img src="/svg/facebook.svg" width={30} className="mt-2"></img>
+            </div>
+            <div className="flex flex-col items-center md:ml-3">
+              <p className="text-start text-3xl font-bold md:text-6xl">4.6</p>
+              <p className="font-secondary font-semibold">400+ Reviews</p>
+            </div>
           </div>
-        </div>
-        <div className="flex flex-col items-center md:flex-row">
-          <div className="bg-primary font-secondary flex h-11 w-12 items-start justify-center rounded-[300%] md:h-22">
-            <img src="/svg/facebook.svg" width={30} className="mt-2"></img>
+          <div className="flex flex-col items-center md:flex-row">
+            <div className="bg-primary font-secondary flex h-11 w-12 items-start justify-center rounded-[300%] md:h-22">
+              <img src="/svg/map.svg" width={30} className="mt-2"></img>
+            </div>
+            <div className="flex flex-col items-center md:ml-3">
+              <p className="text-start text-3xl font-bold md:text-6xl">4.8</p>
+              <p className="font-secondary font-semibold">100+ Reviews</p>
+            </div>
           </div>
-          <div className="flex flex-col items-center md:ml-3">
-            <p className="text-start text-3xl font-bold md:text-6xl">4.6</p>
-            <p className="font-secondary font-semibold">400+ Reviews</p>
-          </div>
-        </div>
-        <div className="flex flex-col items-center md:flex-row">
-          <div className="bg-primary font-secondary flex h-11 w-12 items-start justify-center rounded-[300%] md:h-22">
-            <img src="/svg/map.svg" width={30} className="mt-2"></img>
-          </div>
-          <div className="flex flex-col items-center md:ml-3">
-            <p className="text-start text-3xl font-bold md:text-6xl">4.8</p>
-            <p className="font-secondary font-semibold">100+ Reviews</p>
-          </div>
-        </div>
-        <div className="flex flex-col items-center md:flex-row">
-          <div className="bg-primary font-secondary flex h-11 w-12 items-start justify-center rounded-[300%] md:h-22">
-            <img src="/svg/star.svg" width={25} className="mt-2"></img>
-          </div>
-          <div className="flex flex-col items-center md:ml-3">
-            <p className="text-start text-3xl font-bold md:text-6xl">4.7</p>
-            <p className="font-secondary font-semibold">200+ Reviews</p>
+          <div className="flex flex-col items-center md:flex-row">
+            <div className="bg-primary font-secondary flex h-11 w-12 items-start justify-center rounded-[300%] md:h-22">
+              <img src="/svg/star.svg" width={25} className="mt-2"></img>
+            </div>
+            <div className="flex flex-col items-center md:ml-3">
+              <p className="text-start text-3xl font-bold md:text-6xl">4.7</p>
+              <p className="font-secondary font-semibold">200+ Reviews</p>
+            </div>
           </div>
         </div>
       </div>
